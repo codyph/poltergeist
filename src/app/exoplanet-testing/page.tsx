1021c384777473b2141171df98468a889a59bdbf
Justin Hough <54166410+justinh21@users.noreply.github.com>
2024-10-05 19:58:25 +1000

    Add Exoplanet-testing

---
+++ src/app/exoplanet-testing/page.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import useSWR from "swr";
+import { Exoplanet } from "../fetch-exoplanets/route";
+
+export type ExoplanetDataRequest = {
+  query: string;
+  filter: string;
+};
+
+export default function ExoplanetDataFetcher() {
+  const fetcher = async (url: string, args: ExoplanetDataRequest) => {
+    const params = new URLSearchParams(args);
+    const res = await fetch(`${url}?${params}`)
+    .then((r) => r.json())
+    .then((j) => j.data)
+
+    return res;
+  };
+
+  const requestArgs = {
+    query: "9827",
+    filter: "",
+  };
+
+  const { data, error, isLoading } = useSWR<
+    Exoplanet[],
+    Error,
+    [string, ExoplanetDataRequest]
+  >(["/fetch-exoplanets", requestArgs], ([url, request]) =>
+    fetcher(url, request)
+  );
+
+  return (
+    <div>
+      {error ? (
+        `Error: ${error}`
+      ) : isLoading ? (
+        `Asking for: ${requestArgs.query}, ${requestArgs.filter}...`
+      ) : (
+        <>
+          <h1>Exoplanet Data</h1>
+          <ul>
+            {data &&
+              data.map((planet: Exoplanet, index: number) => (
+                <li key={index}>
+                  Planet name: {planet.pl_name}, Hostname: {planet.hostname},
+                  Year: {planet.disc_year}
+                </li>
+              ))}
+          </ul>
+        </>
+      )}
+    </div>
+  );
+}
---
+++ src/app/fetch-exoplanets/route.ts
@@ -0,0 +1,62 @@
+import { type NextRequest } from "next/server";
+import exoplanets from "../../../public/exoplanets_filtered.json";
+
+export type Exoplanet = {
+  pl_name: string;
+  hostname: string;
+  sy_snum: number;
+  sy_pnum: number;
+  sy_mnum: number;
+  disc_year: number;
+  disc_facility: string;
+  pl_refname: string;
+  pl_orbper: string;
+  pl_orbpererr1: string;
+  pl_orbpererr2: string;
+  pl_orbperlim: number;
+  pl_rade: number;
+  pl_radeerr1: number;
+  pl_radeerr2: number;
+  pl_radelim: number;
+  pl_bmasse: number;
+  pl_bmasseerr1: number;
+  pl_bmasseerr2: number;
+  pl_bmasselim: number;
+  pl_eqt: number;
+  pl_eqterr1: number;
+  pl_eqterr2: number;
+  pl_eqtlim: number;
+  st_teff: number;
+  st_tefferr1: number;
+  st_tefferr2: number;
+  st_tefflim: number;
+  ra: number;
+  dec: number;
+  sy_dist: number;
+  sy_disterr1: number;
+  sy_disterr2: number;
+};
+
+export async function GET(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams;
+  const query = searchParams.get("query");
+  const filter = searchParams.get("filter");
+
+  let data = exoplanets;
+
+  if (query) {
+    data = data.filter((item) => {
+      return (
+        item.pl_name.toLowerCase().includes(query.toLowerCase()) ||
+        item.hostname.toLowerCase().includes(query.toLowerCase())
+      );
+    });
+  }
+
+  if (filter) {
+    // Implement your filter logic here
+    // Example: results = results.filter(item => item.category === filter);
+  }
+
+  return Response.json({ data });
+}
