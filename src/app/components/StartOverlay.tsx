2c77f46e5df7129663725ed30e7847e6264e9a8e
Justin Hough <54166410+justinh21@users.noreply.github.com>
2024-10-06 10:47:11 +1100

    Clean footer and StartOverlay

--- src/app/components/StartOverlay.tsx
+++ src/app/components/StartOverlay.tsx
@@ -8,11 +8,15 @@ export default function StartOverlay({
   setDisplay: (d: boolean) => void;
 }) {
   return (
-    <div className="w-screen h-screen bg-black/70 text-center flex flex-col justify-center px-8">
-      <h1 className="text-[48px] sm:text-[72px] md:text-[96px] leading-tight">The Atlas Project</h1>
-      <h2 className="text-[24px] sm:text-[28px] md:text-[32px] mt-4 sm:mt-2 md:mt-0">Your personal Atlas through the stars</h2>
-      <div className="flex flex-col items-center justify-center mt-6">
-        <Button onClick={() => setDisplay(!display)}>Get started</Button>
+    <div className="w-screen h-screen bg-black/70 text-center flex flex-col items-center justify-center px-8">
+      <h1 className="text-[48px] sm:text-[72px] md:text-[96px] leading-tight">
+        The Atlas Project
+      </h1>
+      <h2 className="text-[24px] sm:text-[28px] md:text-[32px] mt-4 sm:mt-2 md:mt-0">
+        Your personal Atlas through the stars
+      </h2>
+      <div className="flex flex-col items-center justify-center mt-6 text-lg">
+        <Button onClick={() => setDisplay(!display)}>Let&apos;s begin</Button>
       </div>
     </div>
   );
--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,5 +1,3 @@
-import Link from "next/link";
-
 export function SiteFooter() {
   return (
     <footer className="absolute bottom-0 z-10 mt-auto flex w-full items-center justify-center">
