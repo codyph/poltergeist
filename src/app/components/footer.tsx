6620e7292db01f6b9ac5556bb9467b3d1a6160d4
Justin Hough <54166410+justinh21@users.noreply.github.com>
2024-10-05 16:52:51 +1000

    Remove About from footer

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -2,20 +2,17 @@ import Link from "next/link";
 
 export function SiteFooter() {
   return (
-    <footer className="z-10 mt-auto flex w-full items-center justify-center">
+    <footer className="absolute bottom-0 z-10 mt-auto flex w-full items-center justify-center">
       <div className="container text-xs md:text-sm mx-[8%] flex w-full items-center justify-between gap-4 px-0 py-4 lg:px-[2rem]">
         <div className="flex flex-col self-end h-[100%]">
           <div className="whitespace-nowrap">
             &copy; 2024-present Poltergeist
           </div>
-          <div className="">
-            <a href="mailto:theatlasproject@gmail.com">
-            theatlasproject@gmail.com
-            </a>
-          </div>
         </div>
         <div className="flex flex-col items-end text-foreground whitespace-nowrap">
-          <Link href="/">About</Link>
+          <a href="mailto:theatlasproject@gmail.com">
+            theatlasproject@gmail.com
+          </a>
         </div>
       </div>
     </footer>
