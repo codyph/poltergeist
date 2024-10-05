164f5d14e8ca1e58aec737f34c5fa41de1b720f4
Justin Hough <54166410+justinh21@users.noreply.github.com>
2024-10-05 11:10:55 +1000

    Create-next-app init commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,39 @@
+import type { Metadata } from "next";
+import { Overpass_Mono } from "next/font/google";
+import { SiteFooter } from "./components/footer";
+
+export const overpass_mono = Overpass_Mono({
+  subsets: ["latin"],
+  display: "swap",
+});
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "The Atlas Project",
+  description: "Your personal Atlas to the stars.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="en"
+      className={`antialiased overflow-x-hidden`}
+      suppressHydrationWarning
+    >
+      <body className={`${overpass_mono.className} h-screen overflow-x-hidden`}>
+        <div className="flex min-h-[calc(100dvh)] md:min-h-[calc(100dvh)] flex-col">
+          <main className="mx-auto mt-6 flex w-full items-center justify-center pb-3 md:mt-10">
+            <div className="container mx-[8%] w-full px-0 lg:px-[2rem]">
+              {children}
+            </div>
+          </main>
+          <SiteFooter />
+        </div>
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-[144px]">The Atlas Project</h1>
+    </div>
+  );
+}
